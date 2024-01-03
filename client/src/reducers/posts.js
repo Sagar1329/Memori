966354c637fc220f893843b512f9459251316f8e
Your Name <sagargowda1329@gmail.com>
2024-01-03 20:00:35 +0530

    Initial commitj3

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,15 @@
+export default (posts=[],action)=>{
+    switch (action.type)
+    {
+        case "FETCH_ALL":
+            return action.payload;
+        case "CREATE":
+            return [...posts,action.payload] 
+        case "UPDATE":
+            return posts.map((post)=>posts._id==action.payload._id?action.payload:post)
+        case "DELETE":
+            return posts.filter((post)=>post.id!==action.payload)
+        default:
+            return posts;
+    }
+}
